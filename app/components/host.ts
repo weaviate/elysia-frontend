068b7b4883d0d45a655cb20aa73d43fd32ab029c
thomashacker <EdwardSchmuhl@web.de>
2025-07-25 00:55:49 +0200

    fix static link to public resources

--- app/components/host.ts
+++ app/components/host.ts
@@ -5,7 +5,7 @@ export const host =
   process.env.NEXT_PUBLIC_IS_STATIC !== "true" ? "http://localhost:8000" : "";
 
 export const public_path =
-  process.env.NEXT_PUBLIC_IS_STATIC !== "true" ? "/" : "/static";
+  process.env.NEXT_PUBLIC_IS_STATIC !== "true" ? "/" : "/static/";
 
 export const getWebsocketHost = () => {
   if (
