1609cce256978a6bbee13ce4460625518ac641ad
thomashacker <EdwardSchmuhl@web.de>
2025-07-24 16:25:03 +0200

    adjust config

--- next.config.js
+++ next.config.js
@@ -1,6 +1,13 @@
 const nextConfig = {
   output: "export",
   trailingSlash: true,
+  distDir: "out", // Specify output directory
+  // This is key for SPA mode:
+  exportPathMap: async function () {
+    return {
+      '/': { page: '/' }, // Only export the root page
+    }
+  },
   webpack: (config, { isServer }) => {
     // Add a rule to handle .glsl files
     config.module.rules.push({
